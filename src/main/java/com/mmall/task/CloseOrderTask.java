205e32b094eff1813a7237d6046b968279f44e2c
dazou <965510433@qq.com>
2019-01-06 17:36:24 +0800

    redisson lock

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -110,25 +110,25 @@ public class CloseOrderTask {
 
     //使用Redisson实现Redis分布式锁
     @Scheduled(cron = "0 */1 * * * ?") //每一分钟更新一次
-    public void closeOrderTaskV4() {
+    public void closeOrderTaskV4(){
         RLock lock = redissonManager.getRedisson().getLock(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         boolean getLock = false;
         try {
-            if (getLock = lock.tryLock(1, 50, TimeUnit.SECONDS)) {
-                log.info("Reisson获取到分布式锁:{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
-                int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
+            if(getLock = lock.tryLock(0,50, TimeUnit.SECONDS)){
+                log.info("Redisson获取到分布式锁:{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
+                int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
 //                iOrderService.closeOrder(hour);
-            } else {
-                log.info("Reisson没有获取到分布式锁:{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
+            }else{
+                log.info("Redisson没有获取到分布式锁:{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
             }
         } catch (InterruptedException e) {
-            log.error("Redisson获取分布式锁异常",e);
+            log.error("Redisson分布式锁获取异常",e);
         } finally {
-            if (!getLock) {
+            if(!getLock){
                 return;
             }
             lock.unlock();
-            log.info("Redisson释放分布式锁");
+            log.info("Redisson分布式锁释放锁");
         }
     }
 }
